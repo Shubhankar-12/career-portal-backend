2c573a87ec7e3a9353d5598a099da331c183e104
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2025-11-13 10:00:48 +0530

    feat: Remove file-related queries and models; clean up file management code

--- src/db/files/model.ts
+++
@@ -1,5 +0,0 @@
-import { model } from "mongoose";
-import { FileSchema } from "./schema";
-import { IFileDocument } from "./types";
-
-export const FileModel = model<IFileDocument>("file", FileSchema, "files");
--- src/db/queries/index.ts
+++ src/db/queries/index.ts
@@ -1,9 +1,7 @@
 import { CompanyQueries } from "./companyQueries";
-import { FileQueries } from "./filesQueries";
 import { JobQueries } from "./jobQueries";
 import { UserQueries } from "./userQueries";
 
 export const userQueries = new UserQueries();
-export const fileQueries = new FileQueries();
 export const companyQueries = new CompanyQueries();
 export const jobQueries = new JobQueries();
